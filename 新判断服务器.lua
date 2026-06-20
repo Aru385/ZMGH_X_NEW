ba8e8d5698e8e35a86abd8df92889270b3f6b71a
Aru385 <roblox_0513@qq.com>
2026-06-21 01:18:55 +0800

    Add files via upload

---
+++ 新判断服务器.lua
@@ -0,0 +1,55 @@
+--伐木大亨
+if game.PlaceId == 13822889 then
+loadstring(game:HttpGet"https://raw.githubusercontent.com/385j8888/ZOUMAGUIX/refs/heads/main/%E7%B4%85%E4%BC%90%E6%9C%A8.lua")()
+--战争大亨
+elseif game.PlaceId == 4639625707 then
+loadstring(game:HttpGet"https://raw.githubusercontent.com/385j8888/ZOUMAGUIX/refs/heads/main/%E8%B5%B0%E9%A9%AC%E8%A7%82%E8%8A%B1X%E6%88%98%E4%BA%89%E5%A4%A7%E4%BA%A8.lua")()
+--Fisch
+elseif game.PlaceId == 16732694052 then
+loadstring(game:HttpGet"https://raw.githubusercontent.com/385j8888/ZOUMAGUIX/refs/heads/main/%E8%B5%B0%E9%A9%AC%E8%A7%82%E8%8A%B1X%E9%B1%BC.lua")()
+--链
+elseif game.PlaceId == 13977939077 then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/385j8888/ZOUMAGUIX/refs/heads/main/%E8%B5%B0%E9%A9%AC%E8%A7%82%E8%8A%B1X-%E9%93%BE.lua'))()
+--战斗菜鸟
+elseif game.PlaceId == 5734383673 then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/385j8888/ZOUMAGUIX/refs/heads/main/%E8%B5%B0%E9%A9%AC%E8%A7%82%E8%8A%B1X-%E6%88%98%E6%96%97%E4%B8%AD%E7%9A%84%E8%8F%9C%E9%B8%9F.lua'))()
+--nico
+elseif game.PlaceId == 10118559731 then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/385j8888/ZOUMAGUIX/refs/heads/main/%E8%B5%B0%E9%A9%AC%E8%A7%82%E8%8A%B1X-nico%E7%9A%84%E4%B8%8B%E4%B8%80%E4%B8%AA%E6%9C%BA%E5%99%A8%E4%BA%BA.lua'))()
+--nico开放世界
+elseif game.PlaceId == 12629753855 then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/385j8888/ZOUMAGUIX/refs/heads/main/%E8%B5%B0%E9%A9%AC%E8%A7%82%E8%8A%B1X-nico%E7%9A%84%E4%B8%8B%E4%B8%80%E4%B8%AA%E6%9C%BA%E5%99%A8%E4%BA%BA.lua'))()
+--死铁轨
+elseif game.PlaceId == 70876832253163 then
+loadstring(game:HttpGet"https://raw.githubusercontent.com/385j8888/ZOUMAGUIX/refs/heads/main/%E8%B5%B0%E9%A9%AC%E8%A7%82%E8%8A%B1X%E6%AD%BB%E9%93%81%E8%BD%A8.lua")()
+--子弹推车
+elseif game.PlaceId == 130422569332103 then
+loadstring(game:HttpGet"https://raw.githubusercontent.com/385j8888/ZOUMAGUIX/refs/heads/main/%E8%B5%B0%E9%A9%AC%E8%A7%82%E8%8A%B1X%E5%AD%90%E5%BC%B9%E6%8E%A8%E8%BD%A6.lua")()
+--被遗弃
+elseif game.PlaceId == 18687417158 then
+loadstring(game:HttpGet"https://raw.githubusercontent.com/385j8888/ZOUMAGUIX/refs/heads/main/%E8%B5%B0%E9%A9%AC%E8%A7%82%E8%8A%B1X%E8%A2%AB%E9%81%97%E5%BC%83.lua")()
+--生存7天
+elseif game.PlaceId == 121308443347459 then
+loadstring(game:HttpGet"https://raw.githubusercontent.com/385j8888/ZOUMAGUIX/refs/heads/main/%E8%B5%B0%E9%A9%AC%E8%A7%82%E8%8A%B1X%E7%94%9F%E5%AD%987%E5%A4%A9.lua")()
+--生存7天大厅
+elseif game.PlaceId == 17474746614 then
+loadstring(game:HttpGet"https://raw.githubusercontent.com/385j8888/ZOUMAGUIX/refs/heads/main/%E8%B5%B0%E9%A9%AC%E8%A7%82%E8%8A%B1X%E7%94%9F%E5%AD%987%E5%A4%A9%E5%A4%A7%E5%8E%85.lua")()
+--生存7天月球
+elseif game.PlaceId == 77970919775750 then
+loadstring(game:HttpGet"https://raw.githubusercontent.com/385j8888/ZOUMAGUIX/refs/heads/main/%E8%B5%B0%E9%A9%AC%E8%A7%82%E8%8A%B1X%E7%94%9F%E5%AD%987%E5%A4%A9%E6%9C%88%E7%90%83.lua")()
+--造船寻宝
+elseif game.PlaceId == 537413528 then
+loadstring(game:HttpGet"https://raw.githubusercontent.com/385j8888/ZOUMAGUIX/refs/heads/main/%E8%B5%B0%E9%A9%AC%E8%A7%82%E8%8A%B1X%E9%80%A0%E8%88%B9%E5%AF%BB%E5%AE%9D.lua")()
+--森林中的99个夜晚
+elseif game.PlaceId == 126509999114328 then
+loadstring(game:HttpGet"https://raw.githubusercontent.com/385j8888/ZOUMAGUIX/refs/heads/main/%E8%B5%B0%E9%A9%AC%E8%A7%82%E8%8A%B1X%E6%A3%AE%E6%9E%97%E4%B8%AD%E7%9A%8499%E4%B8%AA%E5%A4%9C%E6%99%9A.lua")()
+--种植花园
+elseif game.PlaceId == 126884695634066 then
+loadstring(game:HttpGet"https://raw.githubusercontent.com/385j8888/ZOUMAGUIX/refs/heads/main/%E8%B5%B0%E9%A9%AC%E8%A7%82%E8%8A%B1X%E7%A7%8D%E6%A4%8D%E8%8A%B1%E5%9B%AD.lua")()
+--恶魔学
+elseif game.PlaceId == 18794863104 then
+loadstring(game:HttpGet"https://raw.githubusercontent.com/385j8888/ZOUMAGUIX/refs/heads/main/%E8%B5%B0%E9%A9%AC%E8%A7%82%E8%8A%B1X%E6%81%B6%E9%AD%94%E5%AD%A6.lua")()
+--脑叶分支
+elseif game.PlaceId == 123701320605975 then
+loadstring(game:HttpGet"https://raw.githubusercontent.com/Aru385/ZMGH_X_NEW/refs/heads/main/%E8%84%91%E5%8F%B6%E8%84%9A%E6%9C%AC2.lua")()
+end
